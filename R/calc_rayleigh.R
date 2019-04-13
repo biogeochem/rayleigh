44348b40dbed1c5dc78ad83b54b0fcb844ea6134
Jason Venkiteswaran <jason.venkiteswaran@gmail.com>
2019-04-13 12:42:55 -0400

    Calculate delta of the instantaneous product Plot delta of the instantaneous product Bump version

--- R/calc_rayleigh.R
+++ R/calc_rayleigh.R
@@ -8,6 +8,7 @@
 
 calc_rayleigh <- function(delta_initial, epsilon) {
   fraction_remaining <- seq(0.1, 1 , 0.01)
-  delta <- delta_initial + epsilon * log(fraction_remaining)
-  data.frame(fraction_remaining, delta)
+  delta_residual <- delta_initial + epsilon * log(fraction_remaining)
+  delta_inst_prod <- delta_residual + epsilon
+  data.frame(fraction_remaining, delta_residual, delta_inst_prod)
 }
--- R/plot_rayleigh.R
+++ R/plot_rayleigh.R
@@ -2,18 +2,20 @@
 #'
 #' Plot the Rayleigh Isotope Curve
 #'
-#' This function takes a simple data.frame and plots
+#' This function takes a simple data.frame, gathers it and plots
 #' the delta values of the residual material along a Rayleigh
 #' curve and returns a ggplot object.
 #'
 #' @param df The data.frame returned by calc_rayleigh
 
 plot_rayleigh <- function(df) {
-  ggplot2::ggplot(df, ggplot2::aes(x = fraction_remaining, y = delta)) +
+  df.g <- tidyr::gather(df, key="delta", value="value", -fraction_remaining)
+  ggplot2::ggplot(df.g, ggplot2::aes(x = fraction_remaining, y = value, colour = delta)) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
-    ggplot2::geom_hline(yintercept = df %>% dplyr::filter(fraction_remaining == 1) %>% dplyr::select(delta) %>% as.numeric()) +
+    ggplot2::geom_hline(yintercept = df %>% dplyr::filter(fraction_remaining == 1) %>% dplyr::select(delta_residual) %>% as.numeric()) +
     ggplot2::scale_x_reverse() +
     ggplot2::labs(x = "Fraction remaining",
-         y = "\u3b4 (\u2030)")
+         y = "\u3b4 (\u2030)",
+         colour = "")
 }
