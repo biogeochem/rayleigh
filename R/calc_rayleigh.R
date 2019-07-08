ab21a5efe264b41879e807adfd04bb40d0d8335f
Jason Venkiteswaran <jason.venkiteswaran@gmail.com>
2019-07-08 15:28:59 -0400

    Add hydrogen to options Relabel O as Osmow Change N to 0.0036764 Bump version

--- R/calc_rayleigh.R
+++ R/calc_rayleigh.R
@@ -5,11 +5,13 @@
 #'
 #' @param delta_initial The initial delta value
 #' @param epsilon The isotope enrichement factor
-#' @param element (optional) The element system, C, N, or O
+#' @param element (optional) The element system, C, Hsmow, N, or Osmow
 #' @return data.frame of results
 #' @examples
 #' calc_rayleigh(-20, -15)
 #' calc_rayleigh(-20, -15, "C")
+
+# Standard isotope ratios from https://en.wikipedia.org/wiki/Reference_materials_for_stable_isotope_analysis
 
 calc_rayleigh <- function(delta_initial, epsilon, element = NULL) {
   if (is.null(element)) {
@@ -21,11 +23,14 @@ calc_rayleigh <- function(delta_initial, epsilon, element = NULL) {
   else if (element == "C") {
     Rstd <-  0.011180
   }
-  else if (element == "N") {
-    Rstd <-  0.0036765
+  else if (element == "Hsmow") {
+    Rstd <- 0.000155765
   }
-  else if (element == "O") {
-    Rstd <-  0.002052
+  else if (element == "N") {
+    Rstd <-  0.0036764
+  }
+  else if (element == "Osmow") {
+    Rstd <-  0.00200525
   }
   alpha <- epsilon/1000 + 1
   fraction_remaining <- seq(0.1, 1 , 0.01)
